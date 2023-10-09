b57ae4ab6a5aecba8dabc4f4e44d4f2653b4df26
+dennisngangairungu <+>
2023-10-09 16:44:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+let names=["Guadalupe", "Ollie", "Aki"];
+
+// let messages;
+function writeCards(names,events){
+    let messagesArr=[];
+    for (let i=0;i < names.length; i++){
+        
+       messagesArr.push  (`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+        
+    }
+    return messagesArr
+ 
+    }
+     
+
+writeCards(names,'suprise')
+
+function countDown(){
+    let count=0;
+    while (count < 11){
+        console.log(count++)
+    }
+}
+countDown()
